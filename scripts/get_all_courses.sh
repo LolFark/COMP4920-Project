7989507aabd6d8f927113469093b4440942f1f0e
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-07 19:56:13 +1000

    Cleanup

--- scripts/get_all_courses.sh
+++ scripts/get_all_courses.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Usage: ./get_all_courses.sh < min_subject_areas.txt
+# Usage: ./get_all_courses.sh < subject_areas.txt
 
 export year="2018"
 export ugrad="undergraduate"
@@ -28,29 +28,12 @@ do
             continue
         fi
         wget -q -O- "$handbook_url" | html2text - > handbook_entry.txt.tmp
-        # cat handbook_entry.txt.tmp
         faculty=`egrep "Faculty:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
         co_reqs=`egrep "Corequisite:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
         pre_reqs=`egrep "Prerequisite:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
         excluded=`egrep "Excluded:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
-        # course_name=`egrep "\*{6}.*$course_code\*{6}" handbook_entry.txt.tmp | sed -e 's/\*//g'`
         course_name=`egrep "\*+.*$course_code.*" handbook_entry.txt.tmp | sed -e 's/\*//g'`
-        # echo "==================="
         echo "$course_code |$course_name | $faculty | $co_reqs | $pre_reqs | $excluded | $handbook_url"
     done
 done
 rm handbook_entry.txt.tmp
-
-# course {
-# "code":COMP4920,
-# "name":Ethics,
-# "??description:ethics course,
-# "prereqs":["ds","asjd"],
-# "coreqs":[],
-# "exclusion":[],
-# "level of study":3
-# "handbook_link":url
-# "average_satisfaction":
-# "average_difficulty":
-# "root_comment_history": [comment]
-# }
