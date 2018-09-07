c0675a0154139313657eb3109b367d63c86fb8ce
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-07 19:55:40 +1000

    Get course name

--- scripts/get_all_courses.sh
+++ scripts/get_all_courses.sh
@@ -3,7 +3,8 @@
 # Usage: ./get_all_courses.sh < min_subject_areas.txt
 
 export year="2018"
-export study_level="undergraduate"
+export ugrad="undergraduate"
+export pgrad="postgraduate"
 
 while read -r subject_area
 do
@@ -14,21 +15,31 @@ do
                         egrep "$subject_area[0-9]{4}" |
                         cut -d' ' -f1`
     do
-        export handbook_url="http://legacy.handbook.unsw.edu.au/$study_level/courses/$year/$course_code.html"
+        ugrad_url="http://legacy.handbook.unsw.edu.au/$ugrad/courses/$year/$course_code.html"
+        pgrad_url="http://legacy.handbook.unsw.edu.au/$pgrad/courses/$year/$course_code.html"
+        if curl --output /dev/null --silent --fail -r 0-0 "$ugrad_url"
+        then
+            handbook_url="$ugrad_url"
+        elif curl --output /dev/null --silent --fail -r 0-0 "$pgrad_url"
+        then
+            handbook_url="$pgrad_url"
+        else
+            echo "Course $course_code not found in handbook. Moving on."
+            continue
+        fi
         wget -q -O- "$handbook_url" | html2text - > handbook_entry.txt.tmp
+        # cat handbook_entry.txt.tmp
         faculty=`egrep "Faculty:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
         co_reqs=`egrep "Corequisite:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
         pre_reqs=`egrep "Prerequisite:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
         excluded=`egrep "Excluded:" handbook_entry.txt.tmp | cut -d':' -f2 | sed -e 's/_/ /g'`
-        echo "==================="
-        echo "course_code: $course_code | faculty: $faculty | co_reqs: $co_reqs | pre_reqs: $pre_reqs | excluded: $excluded | handbook_link: $handbook_url"
-        rm handbook_entry.txt.tmp
+        # course_name=`egrep "\*{6}.*$course_code\*{6}" handbook_entry.txt.tmp | sed -e 's/\*//g'`
+        course_name=`egrep "\*+.*$course_code.*" handbook_entry.txt.tmp | sed -e 's/\*//g'`
+        # echo "==================="
+        echo "$course_code |$course_name | $faculty | $co_reqs | $pre_reqs | $excluded | $handbook_url"
     done
-    # exit 1
 done
-
-
-
+rm handbook_entry.txt.tmp
 
 # course {
 # "code":COMP4920,
