e9d0866b46ec10a2a3a5fcd8823ef2804e6fc864
Eric Zhao <ericzhao94@gmail.com>
2018-09-13 14:33:01 +1000

    api and router init

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Courses from '@/components/Courses'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history', // makes it use full url
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/courses',
+      name: 'Courses',
+      component: Courses
     }
   ]
 })
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+// connection between client and server
+
 export default () => {
   return axios.create({
+    // change baseURL for deployment
     baseURL: `http://localhost:8081`
   })
 }
