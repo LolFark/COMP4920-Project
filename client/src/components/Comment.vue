b0bec3a60c01830c891b8fda93bfb7ac30d6ab01
Eric Zhao <ericzhao94@gmail.com>
2018-09-21 01:01:33 +1000

    set up display comment functionality

--- client/src/components/Comment.vue
+++ client/src/components/Comment.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
     <h4>Comments</h4>
+    <ul v-for="comment in comments" v-bind:key="comment">
+      <li>{{comment.content}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import CommentService from '@/services/CommentService'
 export default {
-
+  data () {
+    return {
+      comments: []
+    }
+  },
+  methods: {
+    async getComments () {
+      const response = await CommentService.getComments($route.params.id)
+      this.comments = response.data.comments
+    }
+  }
 }
 </script>
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,10 @@
 const CourseController = require('./controllers/CourseController');
 const AuthenticationController = require('./controllers/AuthenticationController');
+const CommentController = require('./controllers/CommentController');
 
 module.exports = (app) => {
   app.get('/courses', CourseController.getCourses)
   app.post('/register', AuthenticationController.register)
   app.post('/login', AuthenticationController.login)
+  app.get('/comments', CommentController.getComments)
 }
