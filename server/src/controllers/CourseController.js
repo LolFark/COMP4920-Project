a756189b5e0fcba318c64366f6cc23c327ffc38e
stevenlam <steven.lam95@gmail.com>
2018-09-27 12:09:58 +1000

    Add singular course data pull for each course

--- server/src/controllers/CourseController.js
+++ server/src/controllers/CourseController.js
@@ -10,5 +10,17 @@ module.exports = {
         courses: courses
       })
     }).sort({ code: 1 });
+  },
+
+  async getSpecificCourse(req, res) {
+    var code = req.body.code;
+    Course.findOne({code: code}, 'code name pre_reqs co_reqs exclusions faculty course_des handbook_url', function (error, course) {
+      if (error) {
+        console.error(error);
+      }
+      res.send({
+        course: course
+      })
+    });
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,7 @@ const CommentController = require('./controllers/CommentController');
 
 module.exports = (app) => {
   app.get('/courses', CourseController.getCourses)
+  app.post('/course', CourseController.getSpecificCourse)
   app.post('/register', AuthenticationController.register)
   app.post('/login', AuthenticationController.login)
   app.get('/comments', CommentController.getComments)
