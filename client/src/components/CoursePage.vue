f2cb3113a6cf9338f75c8d46c7371f83fec41524
stevenlam <steven.lam95@gmail.com>
2018-09-28 20:48:12 +1000

    Make all courses mentioned in pre-req/co-req/exlusions a link

--- client/src/components/CoursePage.vue
+++ client/src/components/CoursePage.vue
@@ -3,14 +3,14 @@
     <h1>Course {{ $route.params.id }}</h1>
     <p>Feedback</p>
     <p>{{ course }}</p>
-    <table>
+    <table v-if="course !== null">
       <tr class="course_code"><b>Code: </b>{{ course.code }}</tr>
       <tr class="course_name"><b>Name: </b>{{ course.name }}</tr>
       <tr class="course_faculty"><b>Faculty: </b>{{ course.faculty }}</tr>
       <tr class="course_gradlevel"><b>Graduate Level: </b>{{ course.grad_level }}</tr>
-      <tr class="course_coreqs"><b>Corequesite: </b>{{ course.co_reqs }}</tr>
-      <tr class="course_prereqs"><b>Prerequisite: </b>{{ course.pre_reqs }}</tr>
-      <tr class="course_exlusions"><b>Exclusion: </b>{{ course.exclusions }}</tr>
+      <tr class="course_coreqs"><b>Corequesite: </b><span v-html="course.co_reqs">{{ course.co_reqs }}</span></tr>
+      <tr class="course_prereqs"><b>Prerequisite: </b><span v-html="course.pre_reqs">{{ course.pre_reqs }}</span></tr>
+      <tr class="course_exlusions"><b>Exclusion: </b><span v-html="course.exclusions">{{ course.exclusions }}</span></tr>
       <tr class="course_des"><b>Course Description: </b>{{ course.course_des }}</tr>
       <tr class="course_handbookurl"><b>Handbook URL: </b><a v-bind:href="course.handbook_url">Link</a></tr>
     </table>
@@ -41,6 +41,12 @@ export default {
       })
       this.course = response.data.course
       this.course.grad_level = this.course.grad_level.charAt(0).toUpperCase() + this.course.grad_level.substr(1)
+      this.makeLink()
+    },
+    makeLink () {
+      this.course.pre_reqs = this.course.pre_reqs.replace(/([A-Z]{4}[0-9]{4})/g, '<a href="' + '$1' + '">' + '$1' + '</a>')
+      this.course.co_reqs = this.course.co_reqs.replace(/([A-Z]{4}[0-9]{4})/g, '<a href="' + '$1' + '">' + '$1' + '</a>')
+      this.course.exclusions = this.course.exclusions.replace(/([A-Z]{4}[0-9]{4})/g, '<a href="' + '$1' + '">' + '$1' + '</a>')
     }
   }
 }
