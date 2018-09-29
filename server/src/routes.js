30815a611a6393ebf3aa516178d80fe378a31e67
Eric Zhao <ericzhao94@gmail.com>
2018-09-29 15:40:24 +1000

    updated eslint and changed some files to follow airbnb styleguide

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,8 @@ const CourseController = require('./controllers/CourseController');
 const AuthenticationController = require('./controllers/AuthenticationController');
 
 module.exports = (app) => {
-  app.get('/courses', CourseController.getCourses)
-  app.post('/register', AuthenticationController.register)
-  app.post('/login', AuthenticationController.login)
-}
+  app.get('/courses', CourseController.getCourses);
+  app.post('/courses', CourseController.addCourse);
+  app.post('/register', AuthenticationController.register);
+  app.post('/login', AuthenticationController.login);
+};
