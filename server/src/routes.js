7fddfe0bfbfe4eb75a8dde5e990d2f25998dc249
Eric Zhao <ericzhao94@gmail.com>
2018-09-29 22:53:52 +1000

    added tokens and authenticated state

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,8 +2,10 @@ const CourseController = require('./controllers/CourseController');
 const AuthenticationController = require('./controllers/AuthenticationController');
 
 module.exports = (app) => {
-  app.get('/courses', CourseController.getCourses);
+
+  app.get('/courses', CourseController.getCourses)
   app.post('/courses', CourseController.addCourse);
-  app.post('/register', AuthenticationController.register);
-  app.post('/login', AuthenticationController.login);
-};
+  app.post('/course', CourseController.getSpecificCourse)
+  app.post('/register', AuthenticationController.register)
+  app.post('/login', AuthenticationController.login)
+}
