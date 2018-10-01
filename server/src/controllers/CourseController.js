5f8d48fcac72d0846600323c3b8e210b70eb6659
stevenlam <steven.lam95@gmail.com>
2018-10-01 20:46:02 +1000

    Modified to suit ES6 style and add back route for individual pages

--- server/src/controllers/CourseController.js
+++ server/src/controllers/CourseController.js
@@ -3,7 +3,7 @@ const Course = require('../../models/course');
 module.exports = {
   // Fetch all courses
   async getCourses(req, res) {
-    Course.find({}, 'code name grad_level pre_reqs co_reqs exclusions faculty handbook_url', function (error, courses) {
+    Course.find({}, 'code name grad_level pre_reqs co_reqs exclusions faculty handbook_url', (error, courses) => {
       if (error) {
         console.error(error);
       }
@@ -14,13 +14,13 @@ module.exports = {
   },
 
   async getSpecificCourse(req, res) {
-    var code = req.body.code;
-    Course.findOne({code: code}, 'code name grad_level pre_reqs co_reqs exclusions faculty course_des handbook_url', function (error, course) {
+    const code = req.body.code;
+    Course.findOne({code: code}, 'code name grad_level pre_reqs co_reqs exclusions faculty course_des handbook_url', (error, course) => {
       if (error) {
         console.error(error);
       }
       res.send({
-        course: course
+        course,
       })
     });
   },
