118174fc700c9c1d2c048100d501b3a11a0c7c32
Eric Zhao <ericzhao94@gmail.com>
2018-10-02 22:01:39 +1000

    rebasing

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
@@ -17,23 +16,3 @@ const CommentSchema = new Schema({
 
 const Comment = mongoose.model('Comment', CommentSchema);
 module.exports = Comment;
-=======
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var CommentSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    course: {type: Schema.Types.ObjectId, ref: 'Course'},
-    liked_users: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    created: {type: Date},
-    edited: {type: Boolean},
-    replies: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
-    root: {type: Boolean},
-    overall_rating: {type: Number},
-    difficulty: {type: Number},
-    content: String
-})
-
-var Comment = mongoose.model("Comment", CommentSchema);
-module.exports = Comment;
->>>>>>> ea4c8b062c261d34e8b72c4330c90e45ce4de4fa
