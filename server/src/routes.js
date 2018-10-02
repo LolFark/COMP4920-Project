b23c76819e620a846471fd6a603c2ccd82006f3d
stevenlam <steven.lam95@gmail.com>
2018-10-02 10:06:39 +1000

    Resolve conflict

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,7 @@ const CommentController = require('./controllers/CommentController');
 
 module.exports = (app) => {
   app.get('/courses', CourseController.getCourses);
+  app.post('/course', CourseController.getSpecificCourse);
   app.post('/courses', CourseController.addCourse);
   app.post('/register', AuthenticationController.register);
   app.post('/login', AuthenticationController.login);
