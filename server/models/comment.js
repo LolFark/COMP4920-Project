f3cb8e83ff53270bd207cbcafdf526e9c5aef316
Eric Zhao <ericzhao94@gmail.com>
2018-10-02 21:36:12 +1000

    Updated comment model to use reply model

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose.Schema;
 
-var CommentSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    course: {type: Schema.Types.ObjectId, ref: 'Course'},
-    liked_users: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    created: {type: Date},
-    edited: {type: Boolean},
-    replies: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
-    root: {type: Boolean},
-    overall_rating: {type: Number},
-    difficulty: {type: Number},
-    content: String
-})
+const CommentSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  course: { type: Schema.Types.ObjectId, ref: 'Course' },
+  liked_users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  created: { type: Date },
+  edited: { type: Boolean },
+  replies: [{ type: Schema.Types.ObjectId, ref: 'Reply' }],
+  overall_rating: { type: Number },
+  difficulty: { type: Number },
+  content: String,
+});
 
-var Comment = mongoose.model("Comment", CommentSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
 module.exports = Comment;
