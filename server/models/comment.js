78e8dd176035295d131614df59782a3983f45521
Eric Zhao <ericzhao94@gmail.com>
2018-10-02 20:08:25 +1000

    initial change

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,5 +1,6 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
 
 var CommentSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
