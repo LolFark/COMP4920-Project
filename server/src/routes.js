2d7d2d706962b774128a2abcb1eb80deb49c061c
Eric Zhao <ericzhao94@gmail.com>
2018-10-02 21:40:05 +1000

    added new routes for comment functionality

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,17 +3,12 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const CommentController = require('./controllers/CommentController');
 
 module.exports = (app) => {
-<<<<<<< 18569f236f3be03fb88ba2f1f30dd941e5f28fb7
-  app.get('/courses', CourseController.getCourses)
-  app.post('/course', CourseController.getSpecificCourse)
-  app.post('/register', AuthenticationController.register)
-  app.post('/login', AuthenticationController.login)
-  app.get('/comments', CommentController.getComments)
-}
-=======
   app.get('/courses', CourseController.getCourses);
   app.post('/courses', CourseController.addCourse);
   app.post('/register', AuthenticationController.register);
   app.post('/login', AuthenticationController.login);
+  app.get('/comment', CommentController.getComments);
+  app.post('/addComment', CommentController.addComment);
+  app.post('/editComment', CommentController.editComment);
+  app.post('/deletecomment', CommentController.deleteComment);
 };
->>>>>>> updated eslint and changed some files to follow airbnb styleguide
