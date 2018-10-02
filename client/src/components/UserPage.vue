17162b0e871e508137891649320552f871c24ba0
stevenlam <steven.lam95@gmail.com>
2018-10-02 22:15:18 +1000

    Create user page component

---
+++ client/src/components/UserPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="user_page">
+    <h1>User {{ $route.params.username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'user_page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
