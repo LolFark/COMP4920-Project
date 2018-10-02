1abc9166086308a266e11b89500d8188231ce911
stevenlam <steven.lam95@gmail.com>
2018-10-02 22:35:06 +1000

    Display user data pulled from database

--- client/src/components/UserPage.vue
+++ client/src/components/UserPage.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="user_page">
     <h1>User {{ $route.params.username }}</h1>
+    <p>{{ user }}</p>
   </div>
 </template>
 
 <script>
+import UserService from '@/services/UserService'
 export default {
   data () {
     return {
-      name: 'user_page'
+      name: 'user_page',
+      username: this.$route.params.username,
+      user: []
+    }
+  },
+  mounted () {
+    this.getUser()
+  },
+  methods: {
+    async getUser () {
+      const response = await UserService.getUser({
+        username: this.username
+      })
+      this.user = response.data.user
     }
   }
 }
