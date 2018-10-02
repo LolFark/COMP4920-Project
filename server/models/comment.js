e87f33f2de34bef8b067001b2e75ee5438ca9e79
Eric Zhao <ericzhao94@gmail.com>
2018-10-02 21:43:23 +1000

    fix object destructuring typo in comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose.Schema;
+const { Schema } = mongoose;
 
 const CommentSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
