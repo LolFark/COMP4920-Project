609eb7b0269f2ad33f57139e5250207b92cdba5c
stevenlam <steven.lam95@gmail.com>
2018-10-03 13:28:27 +1000

    Added display name and user description to display on user pages

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -3,7 +3,7 @@ const User = require('../../models/user');
 module.exports = {
   async getUser(req, res) {
     const username = req.body.username;
-    User.findOne({username: username}, 'username email password comments liked_comments isAdmin', (error, user) => {
+    User.findOne({username: username}, 'username display_name email password description comments liked_comments isAdmin', (error, user) => {
       if (error) {
         console.error(error);
       }
