94bedab1230ffb57005779522c78eaac311143aa
stevenlam <steven.lam95@gmail.com>
2018-10-09 21:25:02 +1100

    Added comments for better readability

--- client/src/components/UserPage.vue
+++ client/src/components/UserPage.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="user_page">
     <h1>User {{ user.username }}</h1>
+    <!-- If user does not exist in database -->
     <table v-if="user === null">
       <h2><b>USER DOES NOT EXIST</b></h2>
     </table>
+    <!-- If user does exist in database, display user details -->
     <table class="user_info" v-if="user !== null">
       <tr class="name"><b>Name: </b>{{ user.username }}</tr>
       <tr v-if="$store.state.authenticated && $store.state.user.username === user.username"
         class="email"><b>Email: </b>{{ user.email }}</tr>
       <tr class="description"><b>Description: </b>{{ user.description }}</tr>
     </table>
+    <!-- Display any errors if any when changing password or editing user information -->
     <p v-if="errors.length">
       <b>Please correct the following error(s):</b>
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
       </ul>
     </p>
+    <!-- Display message if something success was done -->
     <p v-else-if="message.length"><b>{{ message }}</b></p>
+    <!-- If change password is selected -->
     <div v-if="change_password === true">
       <label for="old_password">Old password: </label>
       <input type="password" id="old_password" v-model="old_password">
@@ -30,6 +35,7 @@
       <v-btn v-on:click="cancel()">Cancel</v-btn>
       <v-btn v-on:click="changePassword()">Save</v-btn>
     </div>
+    <!-- If edit user information is selected -->
     <div v-else-if="edit === true">
       <label for="description">Description: </label>
       <br>
@@ -38,6 +44,7 @@
       <v-btn v-on:click="cancel()">Cancel</v-btn>
       <v-btn v-on:click="changeProfile()">Save</v-btn>
     </div>
+    <!-- If user is logged in and on their own page -->
     <div v-else>
       <p v-if="$store.state.authenticated && $store.state.user.username === user.username">
         <v-btn v-on:click="startChangePassword()">Change Password</v-btn>
@@ -56,14 +63,17 @@ export default {
       username: this.$route.params.username,
       user: [],
 
+      // Edit user information
       edit: false,
       description: '',
 
+      // Change password information
       change_password: false,
       old_password: '',
       new_password: '',
       confirm_password: '',
 
+      // Returned from backend to display
       message: '',
       errors: []
     }
@@ -72,6 +82,7 @@ export default {
     this.getUser()
   },
   methods: {
+    // Gets specific user from database
     async getUser () {
       const response = await UserService.getUser({
         username: this.username
@@ -79,11 +90,18 @@ export default {
       this.user = response.data.user
       this.description = this.user.description
     },
+    // Set data to start password change option
     startChangePassword () {
       this.change_password = true
       this.edit = false
       this.message = ''
     },
+    /* Reset errors each time save is selected
+    Basic front end checking before going to database
+    If passed, pass through username to find in database, old password and new password
+    If an error is returned display it
+    If successful, set everything back to default with success message
+    */
     async changePassword () {
       this.errors = []
       if (this.old_password === this.new_password) {
@@ -105,11 +123,18 @@ export default {
         }
       }
     },
+    // Set data to start edit option
     startChangeProfile () {
       this.edit = true
       this.change_password = false
       this.message = ''
     },
+    /* Reset errors each time save is selected
+    Check if no data has been changed
+    Send username to find in database and data to be updated
+    If an error is returned display it
+    If successfull, set everything back to default with success message
+    */
     async changeProfile () {
       this.errors = []
       if (this.description === this.user.description) {
@@ -128,6 +153,7 @@ export default {
         }
       }
     },
+    // Restore everything back to default
     cancel () {
       this.edit = false
       this.change_password = false
